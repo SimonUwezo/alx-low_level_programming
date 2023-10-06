662016d57be305b5da2c9a1368a9070ed6afdc51
simonuwezo <simonnmwangi@gmail.com>
2023-10-06 15:40:20 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+int digit1, digit2, digit3;
+for (digit1 = 0; digit1 <= 2; digit1++)
+{
+for (digit2 = digit1 + 1; digit2 <= 2; digit2++)
+{
+for (digit3 = digit2 + 1; digit3 <= 2; digit3++)
+{
+putchar(digit1 + '0');
+putchar(digit2 + '0');
+putchar(digit3 + '0');
+if (digit1 != 0 || digit2 != 1 || digit3 != 2)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
