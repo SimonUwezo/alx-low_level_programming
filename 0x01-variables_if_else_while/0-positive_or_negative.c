a17c962dcfa760dfb015ceeb4e066e8d93d03168
simonuwezo <simonnmwangi@gmail.com>
2023-10-06 13:17:53 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
 /**
 * main - Here is the main function
 *
-* Return; always 0
+* Return: always 0
 */
 
 int main(void)
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
 /**
  * main - main function
  *
- * Return : always 0
+ * Return: Always 0
  */
 int main(void)
 {
