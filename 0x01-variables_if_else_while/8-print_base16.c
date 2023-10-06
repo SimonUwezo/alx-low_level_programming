585505124475c2b37e76ca21d3cb96af4382607e
simonuwezo <simonnmwangi@gmail.com>
2023-10-06 12:07:27 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+int i;
+char k;
+for (i = 0 ; i < 10 ; i++)
+putchar(i + '0');
+for (k = 'a' ; k <= 'f' ; k++)
+putchar(k + '0');
+putchar('\n');
+return (0);
+
+}
