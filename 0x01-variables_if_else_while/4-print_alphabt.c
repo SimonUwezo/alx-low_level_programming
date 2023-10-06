fab760bf2042576763a6ed840892afa3181a3a5f
simonuwezo <simonnmwangi@gmail.com>
2023-10-06 10:22:12 -0700

    test

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-
 int main(void)
 {
-    char letter = 'a';
-
-    while (letter <= 'z')
-    {
-        if (letter != 'e' && letter != 'q')
-        {
-            putchar(letter);
-        }
-        letter++;
-    }
-
-    putchar('\n');
-
-    return (0);
+char letter = 'a';
+while (letter <= 'z')
+if (letter != 'e' && letter != 'q')
+putchar(letter);
+letter++;
+putchar('\n');
+return (0);
 }
