71cdf7f8f0c04497fb1c6a9b9dbea8f488d1c302
simonuwezo <simonnmwangi@gmail.com>
2023-10-06 09:33:09 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,12 @@
  *
  * Return: always 0
  */
-
-int main(void);
+int main(void)
 {
-	char i;
-		for (i = 'a' : i <= 'z' ; i++)
-			putchar(i);
-	putchar(0);
-	return (0);
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+putchar(0);
+return (0);
 }
 				
