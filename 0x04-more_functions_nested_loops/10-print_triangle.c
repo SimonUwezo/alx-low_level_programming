e8abc413740ecfda845513c63154c9787df459bc
simonuwezo <simonnmwangi@gmail.com>
2023-10-12 11:44:03 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,6 +15,7 @@ _putchar('\n');
 else
 {
 for (row = 1; row <= size; row++)
+{
 for (spaces = size - row; spaces >= 1; spaces--)
 {
 _putchar(' ');
@@ -24,5 +25,6 @@ for (hashes = 1; hashes <= row; hashes++)
 _putchar('#');
 }
 _putchar('\n');
+}
 }
 }
