0757465ecc36c48d2d88c092efef8075e097f250
simonuwezo <simonnmwangi@gmail.com>
2023-10-16 02:34:54 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,7 +12,7 @@ for (length = 0; s[length] != '\0'; length++);
 z = 0;
 half = length / 2;
 while (half--)
-{temp =s[length - z - 1];
+{temp = s[length - z - 1];
 s[length - z - 1] = s[z];
 s[z] = temp;
 z++;
