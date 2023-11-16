199b57c63da7ac3ba72e86a56cd8f5e933a89072
SimonUwezo <simonnmwangi@gmail.com>
2023-11-16 10:27:42 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include "lists.h"
 /**
-* print_list - function that prints all the elements of a list_t list.
-* @h: list name
-* Return: Number of nodes.
+* print_list - check the code for Holberton School students.
+* @h: name of the list
+* Return: the number of nodes.
 */
 size_t print_list(const list_t *h)
 {
